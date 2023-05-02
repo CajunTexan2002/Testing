a8e2510ff39c201ff7b5158503a3f2f671cc21d2
Tobias Ward <tate@tatesworld.com>
2023-05-02 14:37:13 -0500

    Made Dog Class

---
+++ src/Dog.java
@@ -0,0 +1,26 @@
+
+public class Dog {
+	
+	String name;
+	boolean isCute;
+	
+	public Dog(String name, boolean isCute) {
+		super();
+		this.name = name;
+		this.isCute = isCute;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public boolean isCute() {
+		return isCute;
+	}
+	public void setCute(boolean isCute) {
+		this.isCute = isCute;
+	}
+	
+}
